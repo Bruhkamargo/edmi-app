8c382b23748a79d7a72617e234c2c4d87ddc79fc
bruhkamargo <wwwbrunocamargo@hotmail.com>
2024-02-21 00:37:28 -0300

    Correção Router

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Personalize a porta do servidor
+    open: true, // Abra o navegador automaticamente ao iniciar o servidor
+  },
 })
